7d23a82a6b1b6def283209f2d73a6f855b3812d4
MuiVT <muivt@rikkeisoft.com>
2025-07-28 17:41:00 +0700

    giao dien Dash

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,29 +1,14 @@
 import React, { FC, ReactElement } from 'react';
+
 import { Grid } from '@mui/material';
+import { Sidebar } from '../../components/siderbar/siderbar';
+import { TaskArea } from '../../components/taskArea/taskArea';
 
 export const Dashboard: FC = (): ReactElement => {
   return (
-    <Grid container sx={{ minHeight: '100vh', p: 0, m: 0 }}>
-      <Grid item md={8} sx={{ px: 4 }}>
-        <h2>Content Area</h2>
-      </Grid>
-      <Grid
-        item
-        md={4}
-        sx={{
-          position: 'fixed',
-          top: 0,
-          right: 0,
-          width: '100%',
-          backgroundColor: 'background.paper',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <h2>Slider bar</h2>
-      </Grid>
+    <Grid container minHeight="100vh" p={0} m={0}>
+      <TaskArea />
+      <Sidebar />
     </Grid>
   );
 };
