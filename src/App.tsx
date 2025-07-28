c5cec94088fa73dc968890379503d1a9b39bdf1a
MuiVT <muivt@rikkeisoft.com>
2025-07-28 15:13:14 +0700

    hello world

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ const App: FC = ():ReactElement=>{
     <ThemeProvider theme={customTheme}>
       <CssBaseline>
       <Dashboard/>
+      <h1>Hello world</h1>
       </CssBaseline>
           
     </ThemeProvider >
